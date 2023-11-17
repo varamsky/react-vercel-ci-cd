6eef93d9efe2ac1eacc02cd139821163ef899b01
shubhammindfire <shubhamk@mindfiresolutions.com>
2023-11-17 13:42:08 +0530

    Added vercel project and org id to github and minor change here to test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          React app on Vercel
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
